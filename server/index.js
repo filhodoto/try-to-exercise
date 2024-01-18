eb9a10b5d52ba591b1b5cddafaf36d711395a02d
Gonçalo Ramalho <goncalo@bluecallapp.com>
2024-01-18 11:43:08 +0000

    Add middleware to log path and type of request

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+// Middleware
+app.use((req, res, next) => {
+  console.log(`Path: ${req.path}`);
+  console.log(`Method: ${req.method}`);
+  // without this we won't go to next get/post/etc request
+  next();
+});
+
 // Express app
 app.get('/', (req, res) => {
   res.send({ msg: 'Connected to server' });
