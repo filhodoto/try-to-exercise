def960f252bca3fcec5b04350f36b29103c60f37
Gonçalo Ramalho <goncalo@bluecallapp.com>
2024-01-18 15:14:19 +0000

    Create base for other workouts API requests

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -2,9 +2,33 @@ const express = require('express');
 
 const router = express.Router();
 
-// Get all workouts
+// GET all workouts
 router.get('/', (req, res) => {
   res.send({ msg: 'Get all workouts' });
+});
+
+// GET single workout by :id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.send({ msg: `GET workout with id ${id}` });
+});
+
+// POST a new workout by :id
+router.post('/:id', (req, res) => {
+  const { id } = req.params;
+  res.send({ msg: `POST workout with id ${id}` });
+});
+
+// DELETE single workout by :id
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.send({ msg: `DELETE workout with id ${id}` });
+});
+
+// UPDATE single workout by :id
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  res.send({ msg: `UPDATE workout with id ${id}` });
 });
 
 module.exports = router;
