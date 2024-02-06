1e94617fa15f90e1048b53dad41263f4e8a695e3
Gonçalo Ramalho <goncalo@bluecallapp.com>
2024-02-06 17:25:28 +0000

    Fetch workouts on homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Home = (): JSX.Element => <div className="home">Home component</div>;
+interface Item {
+  _id: string;
+  title: string;
+  reps: number;
+  load: number;
+}
+
+const Home = (): JSX.Element => {
+  const [items, setSetItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch('/api/workouts');
+      console.log('response >>> ', response);
+      // If response was returned correctly
+      if (response.ok) {
+        const itemsJson = await response.json();
+        console.log('itemsJson >>> ', itemsJson);
+        setSetItems(itemsJson);
+      }
+    };
+
+    fetchItems();
+  }, []);
+
+  return (
+    <div className="home">
+      <ul className="items">
+        {items &&
+          items.map((item) => (
+            <li key={item._id} className="item">
+              {item.title}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
