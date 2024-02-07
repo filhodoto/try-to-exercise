edb683c015a8839abb4e2274f253c37546f502d8
Gonçalo Ramalho <goncalo@bluecallapp.com>
2024-02-07 15:54:49 +0000

    Create context with initial state and serve home page with state from context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,6 +16,7 @@ const router = createBrowserRouter([
     ],
   },
 ]);
+
 function App() {
   return (
     <div className="App">
--- frontend/src/context/WorkoutsContext.tsx
+++ frontend/src/context/WorkoutsContext.tsx
@@ -1,19 +1,58 @@
-import { createContext } from 'react';
+import { Item } from 'components/ItemCard';
+import { createContext, useReducer } from 'react';
 
-interface WorkoutsContextProps {}
+// TODO:: Clean this file and sepsrate concepts in different files
+
+// Set action types
+export const CREATE_WORKOUT = 'CREATE_WORKOUT';
+export const SET_WORKOUTS = 'SET_WORKOUTS';
+
+// Set type of actions that can be used, define payload depending on type of action
+type WorkoutsReducerActionTypes =
+  | { type: typeof CREATE_WORKOUT; payload: Item }
+  | { type: typeof SET_WORKOUTS; payload: Item[] };
+
+interface WorkoutsContextProps {
+  workouts: Item[];
+  dispatch: (action: WorkoutsReducerActionTypes) => void;
+}
+
+// Set initial state
+const initialState: WorkoutsContextProps = {
+  workouts: [],
+  dispatch: () => {}, // Placeholder initial dispatch
+};
+
+// Create reducer
+const workoutsReducer = (
+  state: WorkoutsContextProps,
+  action: WorkoutsReducerActionTypes
+) => {
+  switch (action.type) {
+    case SET_WORKOUTS:
+      return { ...state, workouts: action.payload };
+
+    case CREATE_WORKOUT:
+      return { ...state, workouts: [action.payload, ...state.workouts] };
+
+    default:
+      return state;
+  }
+};
 
 // Type context with empty value explaining typescript that this context is empty for the moment
-const WorkoutsContext = createContext<WorkoutsContextProps>({});
+export const WorkoutsContext =
+  createContext<WorkoutsContextProps>(initialState);
 
 export const WorkoutsContextProvider = ({
   children,
-  value,
 }: {
   children: React.ReactNode;
-  value: WorkoutsContextProps;
 }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, initialState);
+
   return (
-    <WorkoutsContext.Provider value={value}>
+    <WorkoutsContext.Provider value={{ ...state, dispatch }}>
       {children}
     </WorkoutsContext.Provider>
   );
